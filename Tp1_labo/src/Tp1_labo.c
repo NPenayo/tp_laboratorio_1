378d060d388c0fbd93be03f29cb6989087373cff
Npenayo <penayonicolas@gmail.com>
2021-04-12 17:34:41 -0300

    First Commit

---
+++ Tp1_labo/src/Tp1_labo.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Tp1_labo.c
+ Author      : Nicolas Alejandro Penayo
+ Version     :
+ Copyright   : Tp 1 de laboratorio 1 / 1E
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
